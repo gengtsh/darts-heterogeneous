38a02721cc8b31f1f0b4391dc3824bdc965072f7
marcosamaris <marcos.amaris@gmail.com>
2018-01-18 21:40:20 -0200

    Edit the script for the sppedup plots

--- R-code/speedup_plots.R
+++ R-code/speedup_plots.R
@@ -1,6 +1,7 @@
 library(ggplot2)
-setwd("/home/marcos/GIT/darts-heterogeneous/others/data/weak/")
+setwd("/home/marcos/darts-heterogeneous/others/data/weak/")
 
+cbbPalette <- c("#000000", "#F0E442", "#56B4E9", "#009E73", "#E69F00", "#0072B2", "#D55E00", "#CC79A7")
 
 df <- data.frame()
 for(j in c("ccsl", "debian", "f4", "hive", "supermicro")){
@@ -28,22 +29,26 @@ for(j in c("ccsl", "debian", "f4", "hive", "supermicro")){
     df <- rbind(df, dataFrame)
 }
 
-Graph <- ggplot(data=df, aes(x=size, y=speedup, group=apps, col=apps, pch=apps)) + 
-    geom_line(size=1.5)+
+df <- df[df$apps != "Sequential",]
+
+Graph <- ggplot(data=df, aes(x=size, y=speedup, group=apps, col=apps, pch=apps, linetype = apps)) + 
+    geom_line(size=1.5,)+
     geom_point(cex=3.5) +
     xlab("Size of the Problem") + 
     theme_bw() +
+    scale_colour_manual(values=cbbPalette) +
     ylab("Speedup" ) +
     theme(plot.title = element_text(family = "Times", face="bold", size=40)) +
     theme(axis.title = element_text(family = "Times", face="bold", size=30)) +
     theme(axis.text  = element_text(family = "Times", face="bold", size=15, colour = "Black")) +
-    # theme(axis.text.x= element_blank()) +
+    scale_x_continuous(breaks=seq(0,50000,5000)) +
+    theme(axis.text.x= element_text(family = "Times", face="bold", size=15, colour = "Black", angle=0, hjust=1)) +
     theme(legend.title  = element_text(family = "Times", face="bold", size=0)) +
     theme(legend.text  = element_text(family = "Times", face="bold", size=18)) +
     theme(legend.direction = "horizontal", 
           legend.position = "bottom",
-          legend.key=element_rect(size=1),
-          legend.key.size = unit(2.25, "lines")) +
+          legend.key=element_rect(size=5),
+          legend.key.size = unit(4, "lines")) +
     guides(col = guide_legend(nrow = 1)) +
      # facet_grid(.~machine, scales="free") +
     facet_wrap(~machine, ncol=1, scales="free") +
@@ -53,22 +58,24 @@ ggsave(paste("./speedUp.pdf",sep=""), Graph, device = pdf, height=18, width=9)
 
 
 df <-  df[df$size >= 17000,]
-Graph <- ggplot(data=df, aes(x=size, y=speedup, group=apps, col=apps, pch=apps)) + 
+Graph <- ggplot(data=df, aes(x=size, y=speedup, group=apps, col=apps, pch=apps, linetype = apps)) + 
     geom_line(size=1.5)+
     geom_point(cex=3.5) +
     xlab("Size of the Problem") + 
     theme_bw() +
+    scale_colour_manual(values=cbbPalette) +
     ylab("Speedup" ) +
     theme(plot.title = element_text(family = "Times", face="bold", size=40)) +
     theme(axis.title = element_text(family = "Times", face="bold", size=30)) +
     theme(axis.text  = element_text(family = "Times", face="bold", size=15, colour = "Black")) +
-    # theme(axis.text.x= element_blank()) +
+    scale_x_continuous(breaks=seq(0,50000,5000)) +
+    theme(axis.text.x= element_text(family = "Times", face="bold", size=15, colour = "Black", angle=0, hjust=1)) +
     theme(legend.title  = element_text(family = "Times", face="bold", size=0)) +
     theme(legend.text  = element_text(family = "Times", face="bold", size=18)) +
     theme(legend.direction = "horizontal", 
           legend.position = "bottom",
-          legend.key=element_rect(size=1),
-          legend.key.size = unit(2.25, "lines")) +
+          legend.key=element_rect(size=5),
+          legend.key.size = unit(4, "lines")) +
     guides(col = guide_legend(nrow = 1)) +
     # facet_grid(.~machine, scales="free") +
     facet_wrap(~machine, ncol=1, scales="free") +
