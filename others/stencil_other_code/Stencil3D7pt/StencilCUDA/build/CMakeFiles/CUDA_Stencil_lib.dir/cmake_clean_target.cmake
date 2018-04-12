19af7637a40dc60b9ca5a45ce14d26d4806469b1
Gengtsh <tgeng@uci.edu>
2018-04-12 11:07:07 -0700

    remove other

--- others/stencil_other_code/Stencil3D7pt/StencilCUDA/build/CMakeFiles/CUDA_Stencil_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libCUDA_Stencil_lib.a"
-)
--- others/stencil_other_code/Stencil3D7pt/StencilCUDA/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tgeng/Stencil/Stencil3D7pt/StencilCUDA
-# Build directory: /home/tgeng/Stencil/Stencil3D7pt/StencilCUDA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
