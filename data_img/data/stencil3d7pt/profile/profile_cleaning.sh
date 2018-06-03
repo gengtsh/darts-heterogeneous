fc06b2a2a449c7e59bde056858cf4fcf5c996607
marcosamaris <marcos.amaris@gmail.com>
2018-06-03 16:58:43 -0300

    Updated the script to format profile data

--- data_img/data/stencil3d7pt/profile/profile_cleaning.sh
+++ data_img/data/stencil3d7pt/profile/profile_cleaning.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+declare -a arr=( "ccsl" )
 
 
-grep -c "Counted" ccsl_opreport_cpu/operf_StencilCudaCpu37_200_200_50_7_1.txt  > var
+declare -a zxy=( "200_200_50" "200_200_100" "200_200_200" "400_400_800" "800_800_200" "800_800_400" "800_800_800" )
 
-tail -n +$(($( cat var)+ 5)) < ccsl_opreport_cpu/operf_StencilCudaCpu37_200_200_100_7_1.txt > temp
+for machine in "${arr[@]}"; do
+     
 
-cat temp | awk '{print $0}'
+    grep -c "Counted" ${machine}_opreport_cpu/operf_StencilCudaCpu37_200_200_50_7_1.txt  > var
 
-rm var temp
+    tail -n +$(($( cat var)+ 5)) < ${machine}_opreport_cpu/operf_StencilCudaCpu37_200_200_100_7_1.txt > temp
+
+    cat temp | awk '{print $1}' | tail -1 
+
+    rm -rf var temp
+done
+
+#"${apps[@]}";
