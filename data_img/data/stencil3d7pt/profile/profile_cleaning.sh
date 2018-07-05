628220e84764f56eb1523892d506c9385b9f89de
marcos <marcos@gpu-ccsl.ime.usp.br>
2018-07-05 01:41:18 -0300

    Updated the script to clean the operf data

--- data_img/data/stencil3d7pt/profile/profile_cleaning.sh
+++ data_img/data/stencil3d7pt/profile/profile_cleaning.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
 
-declare -a arr=( "ccsl" )
+function join { local IFS="$1"; shift; echo "$*"; }
 
+declare -a machines=( "ccsl" )
 
 declare -a zxy=( "200_200_50" "200_200_100" "200_200_200" "400_400_800" "800_800_200" "800_800_400" "800_800_800" )
 
-for machine in "${arr[@]}"; do
-     
+for machine in "${machines[@]}"; do
 
-    grep -c "Counted" ${machine}_opreport_cpu/operf_StencilCudaCpu37_200_200_50_7_1.txt  > var
+	declare -a metrics=("CPU_CLK_UNHALTED" "INST_RETIRED" "LLC_MISSES" "LLC_REFS" "l2_rqsts" "cpl_cycles" "icache" "br_inst_exec" "br_misp_exec" "resource_stalls" "fp_assist" "l2_trans" "mem_uops_retired") 
+	header=`join , ${metrics[@]}` 
+    echo "${machine}, size, ${header}" > tempFileMetric 
+    
+    for sizesZXY in "${zxy[@]}"; do
+		#counter=0
+        grep "Counted" ${machine}_opreport_cpu/operf_StencilCudaCpu37_${sizesZXY}_7_1.txt > varAll
 
-    tail -n +$(($( cat var)+ 5)) < ${machine}_opreport_cpu/operf_StencilCudaCpu37_200_200_100_7_1.txt > temp
+        tempSizeMetric="${machine}, ${sizesZXY},"
+		for metric in "${metrics[@]}"; do
+		        var=`grep -hnr "${metric}" varAll`
 
-    cat temp | awk '{print $1}' | tail -1 
+        #counter=$((counter + 1))
 
-    rm -rf var temp
+        	if [ -n "${var}" ]; then       
+         
+                IFS=':' read -r -a lineMetric <<< $var
+        		countMetric=`echo ${var} | awk '{print $NF}'`
+        
+                #lineMetric=`grep Counted ${machine}_opreport_cpu/operf_StencilCudaCpu37_${zxy}_7_1.txt 
+        
+        		#temp=`tail -n 1 ${machine}_opreport_cpu/operf_StencilCudaCpu37_${zxy}_7_1.txt`       
+        
+                tempMetric=`cat ${machine}_opreport_cpu/operf_StencilCudaCpu37_${zxy}_7_1.txt | awk '{print $0}' | tail -1`
+        
+                tempValueMetric=`echo $tempMetric | awk -v temp=$(($lineMetric*4-2)) ' {print $((temp))*2}'` 
+        
+            else
+                tempValueMetric=0
+            fi
+        
+            tempSizeMetric="$tempSizeMetric $tempValueMetric,"
+    
+        done
+        
+        echo $tempSizeMetric >> tempFileMetric      
+    done
+    #rm -rf var temp
 done
 
 #"${apps[@]}";
