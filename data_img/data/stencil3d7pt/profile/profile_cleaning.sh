0f48f2958da7376ccd562a5fe074a64808bbd4c3
marcos <marcos@gpu-ccsl.ime.usp.br>
2018-07-05 13:37:31 -0300

    Updated the script to format operf data

--- data_img/data/stencil3d7pt/profile/profile_cleaning.sh
+++ data_img/data/stencil3d7pt/profile/profile_cleaning.sh
@@ -2,22 +2,23 @@
 
 function join { local IFS="$1"; shift; echo "$*"; }
 
-declare -a machines=( "ccsl" )
+declare -a Machines=( "ccsl" )
 
-declare -a zxy=( "200_200_50" "200_200_100" "200_200_200" "400_400_800" "800_800_200" "800_800_400" "800_800_800" )
+declare -a ZXY=( "200_200_50" "200_200_100" "200_200_200" "400_400_800" "800_800_200" "800_800_400" "800_800_800" )
 
-for machine in "${machines[@]}"; do
+for machine in "${Machines[@]}"; do
 
-	declare -a metrics=("CPU_CLK_UNHALTED" "INST_RETIRED" "LLC_MISSES" "LLC_REFS" "l2_rqsts" "cpl_cycles" "icache" "br_inst_exec" "br_misp_exec" "resource_stalls" "fp_assist" "l2_trans" "mem_uops_retired") 
-	header=`join , ${metrics[@]}` 
-    echo "${machine}, size, ${header}" > tempFileMetric 
+	declare -a Metrics=("CPU_CLK_UNHALTED" "INST_RETIRED" "LLC_MISSES" "LLC_REFS" "l2_rqsts" "cpl_cycles" "icache" "br_inst_exec" "br_misp_exec" "resource_stalls" "fp_assist" "l2_trans" "mem_uops_retired") 
+	header=`join , ${Metrics[@]}` 
+    echo "machine, size, ${header}" > tempFileMetric 
     
-    for sizesZXY in "${zxy[@]}"; do
+    for sizesZXY in "${ZXY[@]}"; do
 		#counter=0
         grep "Counted" ${machine}_opreport_cpu/operf_StencilCudaCpu37_${sizesZXY}_7_1.txt > varAll
 
         tempSizeMetric="${machine}, ${sizesZXY},"
-		for metric in "${metrics[@]}"; do
+        tempMetric=`cat ${machine}_opreport_cpu/operf_StencilCudaCpu37_${sizesZXY}_7_1.txt | awk  '{print $0}' | tail -1`
+		for metric in "${Metrics[@]}"; do
 		        var=`grep -hnr "${metric}" varAll`
 
         #counter=$((counter + 1))
@@ -31,21 +32,23 @@ for machine in "${machines[@]}"; do
         
         		#temp=`tail -n 1 ${machine}_opreport_cpu/operf_StencilCudaCpu37_${zxy}_7_1.txt`       
         
-                tempMetric=`cat ${machine}_opreport_cpu/operf_StencilCudaCpu37_${zxy}_7_1.txt | awk '{print $0}' | tail -1`
         
-                tempValueMetric=`echo $tempMetric | awk -v temp=$(($lineMetric*4-2)) ' {print $((temp))*2}'` 
+                tempValueMetric=`echo $tempMetric | awk -v temp=$(($lineMetric*4-2)) ' {print $((temp))}'`
+                tempValueMetric=`echo $((${tempValueMetric} * ${countMetric}))`
+                var=""
         
             else
                 tempValueMetric=0
             fi
         
             tempSizeMetric="$tempSizeMetric $tempValueMetric,"
-    
+#            var=""
         done
         
         echo $tempSizeMetric >> tempFileMetric      
     done
-    #rm -rf var temp
+    mv tempFileMetric ${machine}_opreport_cpu/tempFileMetric.csv
+    rm -rf varAll
 done
 
 #"${apps[@]}";
