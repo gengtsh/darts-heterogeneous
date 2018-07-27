28d80e44da9b205967516ecf27c30db84522b36b
marcosamaris <marcos.amaris@gmail.com>
2018-07-27 19:10:16 -0300

    Modified the script of machine learning models with streams data

--- R-code/streams_ML.R
+++ R-code/streams_ML.R
@@ -40,4 +40,71 @@ for(version in c(versions)){
 }
 
 
-dataTS <- data.frame(read.csv(paste("./ScriptAndOutput/GPUExecutionTimeSAndNS/nvprofTraces_overlap.csv", sep="")))
+PP_data_gpu <- data.frame()
+for(metric in c("cudadriver","gpu_running","kernel","overlap","transDtoH","transHtoD")){
+    dataTemp <- data.frame(Metrics=metric, read.csv(paste("./ScriptAndOutput/GPUExecutionTimeSAndNS/nvprof_postpose/nvprofTraces_", metric, ".csv", sep="")))
+    
+    for(machine in machines){
+            dataNew <- dataTemp[dataTemp$sever == machine, 1:5]
+            dataIter <- dataTemp[dataTemp$sever == machine, 6:15]
+            PP_data_gpu_temporal <- data.frame()
+            for (i in 1:10){
+                PP_data_gpu_temporal <- rbind(PP_data_gpu_temporal, cbind(dataNew, iteration=i, time=dataIter[,paste("its.", i, sep="")]))
+            }
+            PP_data_gpu <- rbind(PP_data_gpu, PP_data_gpu_temporal)
+    }
+}
+
+
+PP_data_gpu <- PP_data_gpu[PP_data_gpu$iteration != 10,]
+PP_data_gpu$time <- PP_data_gpu$time/max(PP_data_gpu$time)
+
+par("mar")
+par(mar=c(1,1,1,1), mfrow=c(6,2))
+# png("./plot_postpone_data.png", width = 600, height = 800)
+
+for(metric in c("cudadriver","gpu_running","kernel","overlap","transDtoH","transHtoD")){
+    for(version in versions){
+        
+        trainingSet <- PP_data_gpu[PP_data_gpu$stream.Y.NO == version & PP_data_gpu$Metrics == metric &
+                                       PP_data_gpu$iteration <= 5 ,]
+        
+        testSet <- PP_data_gpu[PP_data_gpu$stream.Y.NO == version & PP_data_gpu$Metrics == metric &
+                                       PP_data_gpu$iteration >= 7 & PP_data_gpu$iteration <= 9 ,]
+        
+        trainingSet$Metrics <- NULL
+        trainingSet$stream.Y.NO <- NULL
+        trainingSet$size <- NULL
+        trainingSet$sever <- NULL
+        trainingSet[apply(trainingSet, 2, is.infinite)] <- 0
+        trainingSet[apply(trainingSet, 2, is.na)] <- 0
+        trainingSet <- trainingSet[, apply(trainingSet, 2, function(v) var(v, na.rm = TRUE) != 0)]
+        
+        testSet$Metrics <- NULL
+        testSet$stream.Y.NO <- NULL
+        testSet$size <- NULL
+        testSet$sever <- NULL
+        testSet[apply(testSet, 2, is.infinite)] <- 0
+        testSet[apply(testSet, 2, is.na)] <- 0
+        testSet <- testSet[, apply(testSet, 2, function(v) var(v, na.rm = TRUE) != 0)]
+        durationReal <- testSet$time
+        testSet$time = NULL
+        
+        fit.model <- lm((trainingSet$time)~., data=trainingSet)
+        predictTime <- predict(fit.model, testSet)
+
+        accuracy <- predictTime/durationReal
+        
+        boxplot(accuracy, main=paste(metric, " in version ",version, sep="" ))
+    }
+}
+# dev.off()
+
+data_Overlap <- data.frame(read.csv(paste("./ScriptAndOutput/GPUExecutionTimeSAndNS/nvprofTraces_overlap.csv", sep="")))
+
+
+data_gpu_streams <- as.data.frame(data_gpu_streams)
+data_gpu_streams$total_CUDA_functions <- NULL
+
+
+# fit <- lm(data_gpu_streams$total_running_time~., data = data_gpu_streams)
